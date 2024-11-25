bf56b635987d26b7f0bc128dc1d02fd7c296cd9a
alexanderhuahuacccapacc <alexander123huahuaccapa@gmail.com>
2024-11-25 02:06:05 -0500

    wasaaaa

---
+++ Bautismo/src/main/java/BAUTISMO/Bautismo/BautismoApplication.java
@@ -0,0 +1,13 @@
+package BAUTISMO.Bautismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BautismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BautismoApplication.class, args);
+	}
+
+}
---
+++ Bautismo/src/test/java/BAUTISMO/Bautismo/BautismoApplicationTests.java
@@ -0,0 +1,13 @@
+package BAUTISMO.Bautismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BautismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
